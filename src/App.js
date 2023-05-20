77f6c1777c5b65a74dcfec069415c6f6bbf241f5
deanglane <dglaneto@gmail.com>
2023-05-20 12:02:57 -0400

    css setup norm base

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Flash It!</h1>
+      <p>The app that will help you study harder</p>
+    </div>
+  );
+}
+
+export default App;
