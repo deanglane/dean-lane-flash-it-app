30c3afdde64cdb14be389ac242fb7bb81a817d85
deanglane <dglaneto@gmail.com>
2023-05-20 17:35:04 -0400

    unsplash for images for articles

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
             <h1 className="introTitleH1">Flash It</h1>
             <h2 className="introTitleH2">A card based memory app</h2>
             <p>Welcome to Flash It!. A web-based flash card app to help with your memory recollection.</p> 
-            <p>Are you tired of traditional study methods that feel mundane and ineffective? Look no further! Introducing the power of flashcards – the time-tested technique that has helped countless learners around the world achieve academic success..
+            <p>Are you tired of traditional study methods that feel mundane and ineffective? Look no further! Introducing the power of flashcards. The time-tested technique that has helped countless learners around the world achieve academic success..
             </p>
             <div className="introBtns">
               <a className="btn" href="#articles">Learn More</a>
@@ -69,36 +69,38 @@ function App() {
 
               <div className="artCard">
                 <div className="artCardHeader">
-                  <img src={artCardImg} alt="Books and notepad on a desk" />
+                  <img src={require('./assets/aaron-burden-QJDzYT_K8Xg-unsplash.jpg')} alt="Photo by Aaron-Burden " />
                 </div>
                 <div className="artCardBody">
                   <h4>Boosting Memory Recollection:</h4>
                   <h5>Unleashing the Power of Flash Card Technique</h5>
-                  <p>In the realm of memory recollection, the flash card technique has proven to be a powerful tool. From students aiming...</p>
+                  <p>In the realm of memory recollection, the flash card technique has proven to be a powerful tool. From students aiming to excel in exams to professionals seeking to retain vast amounts of information, flash cards offer an effective and efficient way...</p>
                   <button>read more</button>
                 </div>
               </div>
 
               <div className="artCard">
                 <div className="artCardHeader">
-                  <img src={artCardImg} alt="Books and notepad on a desk" />
+                  <img src={require('./assets/jo-szczepanska-bjemWZcNF34-unsplash.jpg')} alt="Photo by Jo Szczepanska" />
                 </div>
+                
                 <div className="artCardBody">
-                  <h4>Boosting Memory Recollection:</h4>
-                  <h5>Unleashing the Power of Flash Card Technique</h5>
-                  <p>In the realm of memory recollection, the flash card technique has proven to be a powerful tool. From students aiming...</p>
+                  <h4>Unleashing the Power of Visual Memory:</h4>
+                  <h5>How Humans Retain Memories Using Images</h5>
+                  <p>Throughout our lives, our minds become a treasure trove of memories, storing countless experiences and information. Among the various tools that aid in memory retention, images hold a remarkable place. From childhood recollections...</p>
                   <button>read more</button>
                 </div>
               </div>
 
               <div className="artCard">
                 <div className="artCardHeader">
-                  <img src={artCardImg} alt="Books and notepad on a desk" />
+                  <img src={require('./assets/afonso-coutinho-2mN0Bxrnr34-unsplash.jpg')} alt="Photo by Afonso Coutinho" />
                 </div>
+
                 <div className="artCardBody">
-                  <h4>Boosting Memory Recollection:</h4>
-                  <h5>Unleashing the Power of Flash Card Technique</h5>
-                  <p>In the realm of memory recollection, the flash card technique has proven to be a powerful tool. From students aiming...</p>
+                  <h4>Striking a Balance:</h4>
+                  <h5>Study Duration and the Vitality of Breaks</h5>
+                  <p>The pursuit of effective studying involves understanding the delicate interplay between study duration and the value of taking breaks. In this article, we delve into the optimal length of study sessions, explore the science behind the importance of breaks...</p>
                   <button>read more</button>
                 </div>
               </div>
