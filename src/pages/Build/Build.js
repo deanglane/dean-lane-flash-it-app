36ea0da47d62d6bd60eee3ee00fb39f2e5347bfe
deanglane <dglaneto@gmail.com>
2024-01-14 09:12:34 -0500

    Styled build section

--- src/pages/Build/Build.js
+++ src/pages/Build/Build.js
@@ -1,44 +1,44 @@
-import React, { useState } from "react"
-import firebase from "../../util/firebase.js"
-import { getDatabase, ref, push } from "firebase/database"
-import { getAuth } from "firebase/auth"
-import { Link } from "react-router-dom"
+import React, { useState } from "react";
+import firebase from "../../util/firebase.js";
+import { getDatabase, ref, push } from "firebase/database";
+import { getAuth } from "firebase/auth";
+import { Link } from "react-router-dom";
 
 const Build = () => {
   const [userInput, setUserInput] = useState({
     back: "",
     front: "",
-  })
+  });
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const auth = getAuth()
-    const database = getDatabase(firebase)
+    const auth = getAuth();
+    const database = getDatabase(firebase);
 
-    const dbRef = ref(database, `/profiles/${auth.currentUser.uid}/cards`)
+    const dbRef = ref(database, `/profiles/${auth.currentUser.uid}/cards`);
 
-    push(dbRef, userInput)
+    push(dbRef, userInput);
 
     setUserInput({
       back: "",
       front: "",
-    })
-    alert("Card Submitted")
-  }
+    });
+    alert("Card Submitted");
+  };
   const handleChange = (e) => {
     //spreading existing state back into new state value
-    // pushes each new card to the ned of the array
-    setUserInput({ ...userInput, [e.target.name]: e.target.value })
-  }
+    // pushes each new card to the end of the array
+    setUserInput({ ...userInput, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <section className="formSection">
         <div className="buildHeader wrapper">
-          <h2>Card Builder</h2>
+          <h2>Topic Builder</h2>
           <div className="navButtons">
             <Link to="/build">
-              <button className="btn">Build</button>
+              <button className="btn btn1">Build</button>
             </Link>
             <Link to="/review">
               <button className="btn">Review</button>
@@ -47,23 +47,29 @@ const Build = () => {
         </div>
         <div className="formContain">
           <form onSubmit={handleSubmit} action="submit">
-            <label htmlFor="front">Front</label>
-            <textarea
-              name="front"
-              id="front"
-              onChange={handleChange}
-              value={userInput.front}
-              placeholder="Enter your question or statement here"
-            ></textarea>
+            <div className="inputContainer">
+              <div className="front">
+                <label htmlFor="front">Front</label>
+                <textarea
+                  name="front"
+                  id="front"
+                  onChange={handleChange}
+                  value={userInput.front}
+                  placeholder="Enter your question or statement here"
+                ></textarea>
+              </div>
 
-            <label htmlFor="back">Back</label>
-            <textarea
-              name="back"
-              id="back"
-              onChange={handleChange}
-              value={userInput.back}
-              placeholder="enter your answer here"
-            ></textarea>
+              <div className="back">
+                <label htmlFor="back">Back</label>
+                <textarea
+                  name="back"
+                  id="back"
+                  onChange={handleChange}
+                  value={userInput.back}
+                  placeholder="enter your answer here"
+                ></textarea>
+              </div>
+            </div>
 
             <div className="formButtons">
               <button className="btn">Clear</button>
@@ -75,7 +81,7 @@ const Build = () => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Build
+export default Build;
--- src/pages/Review/Review.js
+++ src/pages/Review/Review.js
@@ -1,59 +1,51 @@
 // Review.js
 
 // import Modules
-import { getDatabase, 
-  ref, 
-  get } from 'firebase/database';
-import firebase from '../../util/firebase.js';
+import { getDatabase, ref, get } from "firebase/database";
+import firebase from "../../util/firebase.js";
 import { getAuth } from "firebase/auth";
-import { useEffect, 
-  useState } from 'react';
+import { useEffect, useState } from "react";
 
 // import components
-import Cards from './Cards.js';
-import { Link } from 'react-router-dom';
+import Cards from "./Cards.js";
+import { Link } from "react-router-dom";
 
-  const Review = ({ displayName }) => {
-  
+const Review = ({ displayName }) => {
   const [flashCards, setFlashCards] = useState([]);
-  const [checkCards, setCheckCards] = useState('');
-  
+  const [checkCards, setCheckCards] = useState("");
+
   const auth = getAuth();
   const userId = auth.currentUser.uid;
-  
+
   useEffect(() => {
     const database = getDatabase(firebase);
     const dbRef = ref(database, `/profiles/${userId}/cards`);
-    
-    get(dbRef).then((snapshot) => {
-      if (snapshot.exists()) {
-        setCheckCards(false)
-        const newState = [];
-        const cards = snapshot.val()
-        
-        for (const key in cards) {
-          const cardData = {
-            key: key,
-            card: cards[key]
-          };
-          
-          newState.push(cardData);
-          
+
+    get(dbRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          setCheckCards(false);
+          const newState = [];
+          const cards = snapshot.val();
+
+          for (const key in cards) {
+            const cardData = {
+              key: key,
+              card: cards[key],
+            };
+
+            newState.push(cardData);
+          }
+
+          setFlashCards(newState);
+        } else {
+          setCheckCards(true);
         }
-
-        setFlashCards(newState);
-
-      } else {
-
-        setCheckCards(true)
-
-      }
-    }).catch((error) => {
-      alert(error);
-    })
-
-  }, [userId])
-
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, [userId]);
 
   return (
     <>
@@ -61,29 +53,30 @@ import { Link } from 'react-router-dom';
         <div className="flashCards">
           <div className="reviewHeader">
             <h2>Review</h2>
-            <Link to='/build'>
-              <button className='btn'>Build</button>
-            </Link>
-            <Link to='/review'>
-              <button className='btn'>Review</button>
-            </Link>
+            <div className="reviewNavBtn">
+              <Link to="/build">
+                <button className="btn btn1">Build</button>
+              </Link>
+              <Link to="/review">
+                <button className="btn">Review</button>
+              </Link>
+            </div>
           </div>
           {/* ternary expression to check if there are cards created in the database, If false a message will be displayed, if true the cards will display */}
-          {
-            checkCards ? (
-              <div className="noCards">
-                <h3>No Cards Created.</h3>
-                <p>Go to the build page and start creating your flash it deck now!</p>
-              </div>
+          {checkCards ? (
+            <div className="noCards">
+              <h3>No Cards Created.</h3>
+              <p>
+                Go to the build page and start creating your flash it deck now!
+              </p>
+            </div>
           ) : (
-              <Cards flashCards={flashCards}/>
-          )
-          }
-
+            <Cards flashCards={flashCards} />
+          )}
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
 export default Review;
