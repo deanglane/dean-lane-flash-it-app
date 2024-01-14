f749ee27ec2e688ce5443a729d5ecf1c74663e97
deanglane <dglaneto@gmail.com>
2024-01-14 07:55:26 -0500

    Updated Footer

--- src/pages/Build/Build.js
+++ src/pages/Build/Build.js
@@ -1,35 +1,35 @@
-import React, { useState } from 'react';
-import firebase from '../../util/firebase.js';
-import { getDatabase, ref, push } from 'firebase/database';
-import { getAuth } from "firebase/auth";
-import { Link } from 'react-router-dom';
+import React, { useState } from "react"
+import firebase from "../../util/firebase.js"
+import { getDatabase, ref, push } from "firebase/database"
+import { getAuth } from "firebase/auth"
+import { Link } from "react-router-dom"
 
 const Build = () => {
-
   const [userInput, setUserInput] = useState({
-    back: '',
-    front: ''
+    back: "",
+    front: "",
   })
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    const auth = getAuth();
-    const database = getDatabase(firebase);
+    const auth = getAuth()
+    const database = getDatabase(firebase)
 
-    const dbRef = ref(database, `/profiles/${auth.currentUser.uid}/cards`);
+    const dbRef = ref(database, `/profiles/${auth.currentUser.uid}/cards`)
 
-    push(dbRef, userInput);
+    push(dbRef, userInput)
 
     setUserInput({
-      back: '',
-      front: ''
-    });
-    alert("Card Submitted");
+      back: "",
+      front: "",
+    })
+    alert("Card Submitted")
   }
   const handleChange = (e) => {
     //spreading existing state back into new state value
-    setUserInput({ ...userInput, [e.target.name]: e.target.value });
+    // pushes each new card to the ned of the array
+    setUserInput({ ...userInput, [e.target.name]: e.target.value })
   }
   return (
     <>
@@ -37,25 +37,24 @@ const Build = () => {
         <div className="buildHeader wrapper">
           <h2>Card Builder</h2>
           <div className="navButtons">
-            <Link to='/build'>
-              <button className='btn'>Build</button>
+            <Link to="/build">
+              <button className="btn">Build</button>
             </Link>
-            <Link to='/review'>
-              <button className='btn'>Review</button>
+            <Link to="/review">
+              <button className="btn">Review</button>
             </Link>
           </div>
         </div>
-        <div className='formContain'>
+        <div className="formContain">
           <form onSubmit={handleSubmit} action="submit">
-
             <label htmlFor="front">Front</label>
             <textarea
               name="front"
               id="front"
               onChange={handleChange}
               value={userInput.front}
-              placeholder="Enter your question or statement here">
-            </textarea>
+              placeholder="Enter your question or statement here"
+            ></textarea>
 
             <label htmlFor="back">Back</label>
             <textarea
@@ -63,14 +62,15 @@ const Build = () => {
               id="back"
               onChange={handleChange}
               value={userInput.back}
-              placeholder="enter your answer here">
-            </textarea>
+              placeholder="enter your answer here"
+            ></textarea>
 
             <div className="formButtons">
-              <button className='btn'>Clear</button>
-              <button className='btn' type="submit">Submit</button>
+              <button className="btn">Clear</button>
+              <button className="btn" type="submit">
+                Submit
+              </button>
             </div>
-
           </form>
         </div>
       </section>
@@ -78,4 +78,4 @@ const Build = () => {
   )
 }
 
-export default Build;
+export default Build
